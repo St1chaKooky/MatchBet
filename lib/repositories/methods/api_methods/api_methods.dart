804c6764218c32ccb17b974132c9101dab41b192
wov1a <vova.stoan2006@gmail.com>
2023-09-14 13:20:30 +0300

    выводим фикстур в консоль

--- lib/repositories/methods/api_methods/api_methods.dart
+++ lib/repositories/methods/api_methods/api_methods.dart
@@ -10,22 +10,32 @@ class ApiMethods {
           await ApiMeneger().getApi(); //получаем наш джсон в виде обьекта
       if (myResponse is MyResponse) {
         final matchModels = myResponse
-            .response; // масив Моделей матча (.response означает что мы оброщаемся к полю респонс)
-        print(matchModels);
+            .response; // масив Моделей матча [fixture,league,goals,score...] (.response означает что мы оброщаемся к полю респонс)
         if (matchModels != null) {
-          // Итерируемся по списку matchModels и выводим значения поля fixture
+          // Итерируемся по списку matchModels , каждый элемент это
+          // {
+          // "fixture": {},
+          // "league": {},
+          // "teams": {},
+          // "goals": {},
+          // "score": {}
+          // }
+
           for (var matchModel in matchModels) {
+            final league = matchModel.league;
+            final teams = matchModel.teams;
+            final goals = matchModel.goals;
             final fixture = matchModel.fixture;
             if (fixture != null) {
               // Выводим значения полей fixture
-              print('Fixture ID: ${fixture.id}');
+              print('ID: ${fixture.id}');
               print('Referee: ${fixture.referee}');
               print('Timezone: ${fixture.timezone}');
               print('Date: ${fixture.date}');
+              print('Timestamp: ${fixture.timestamp}');
               print('Periods: ${fixture.periods}');
-
-              // Выводите остальные поля fixture по аналогии
-              print('-----');
+              print('Venue: ${fixture.venue}');
+              print('Status: ${fixture.status}');
             }
           }
         } else {
